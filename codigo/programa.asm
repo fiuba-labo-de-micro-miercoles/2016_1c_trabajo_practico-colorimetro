5f353fe1751cf3257f8e01fe2a33d5aa8417d26b
Matias <matias.anas@gmail.com>
2016-05-18 16:50:23 -0300

    Ahora envia cadenas por puerto serie.

--- codigo/programa.asm
+++ codigo/programa.asm
@@ -31,13 +31,18 @@ main:	ldi 	r16,LOW(RAMEND)
 
 	rcall	uart_init
 
-loop:	rcall	transmitir
+loop:	ldiw	Z,(MENSAJE*2)	
+	rcall	tx_string
 	sbi	PORTD,PD7
 	rcall	delay
 	cbi	PORTD,PD7
 	rcall 	delay
 	rjmp	loop
 	
+;************************************************************************************
+
+; Rutinas de comunicacion serie:
+
 .equ	BAUD_RATE = 103	; 9600
 	
 uart_init:
@@ -46,17 +51,37 @@ uart_init:
 	outi	UCSR0B, (1<<RXEN0)|(1<<TXEN0)
 	outi    UCSR0C, (1<<USBS0)|(3<<UCSZ00)
 	ret
+
+; tx_string: Transmite una cadena hubicada en ROM.
+; Entrada: Z: Posicion donde comienza la cadena.	
 	
-transmitir:
+tx_string:
+	push	r16
+tx_string_loop:
+	lpm	r16, Z+
+	cpi	r16, 0
+	breq	fin_tx_string
+	rcall	tx_byte
+	rjmp	tx_string_loop
+fin_tx_string:
+	pop	r16	
+	ret
+	
+; tx_byte: Tansmite un byte.
+; Entrada: r16: Byte a transmitir.	
+	
+tx_byte:
 	rcall	esperar_buffer_de_tx_vacio
-	ldi	r16, 'A'
 	sts	UDR0, r16
 	ret
 	
 esperar_buffer_de_tx_vacio:
+	push	r16
+esperar_buffer_de_tx_vacio_loop:
 	lds	r16, UCSR0A
 	sbrs	r16, UDRE0
-	rjmp	esperar_buffer_de_tx_vacio
+	rjmp	esperar_buffer_de_tx_vacio_loop
+	pop	r16
 	ret
 	
 uart_rx_complete_isr:
@@ -64,6 +89,8 @@ uart_rx_complete_isr:
 	
 uart_reg_vacio_isr:
 	reti
+	
+;************************************************************************************
 	
 delay:
 	ldi	r17,255
@@ -79,3 +106,7 @@ delay_loop_3:
 	dec	r17
 	brne	delay_loop_1
 	ret
+	
+;************************************************************************************
+	
+MENSAJE:	.db	"hola", 13, 10, 0
